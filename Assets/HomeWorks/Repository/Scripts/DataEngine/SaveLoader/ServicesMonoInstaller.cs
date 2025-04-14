b542db94a4f2ad35cd8c8f1fec7d870837c6e368
vic-meateater <victor.myasoedov@gmail.com>
2025-04-14 22:44:51 +0500

    in progress

---
+++ Assets/HomeWorks/Repository/Scripts/DataEngine/SaveLoader/ServicesMonoInstaller.cs
@@ -0,0 +1,22 @@
+﻿using GameEngine;
+using UnityEngine;
+using Zenject;
+
+namespace DataEngine
+{
+    public class MonoInstaller : MonoInstaller
+    {
+        [SerializeField] private UnitManager _unitManager;
+        [SerializeField] private ResourceService _resourceService;
+        
+        public override void InstallBindings()
+        {
+            var units = FindObjectsOfType<Unit>();
+            var resources = FindObjectsOfType<Resource>();
+            
+            Container.Bind<SaveLoader>().FromNew().AsSingle().WithArguments(units, resources).NonLazy();
+            Container.Bind<UnitManager>().FromInstance(_unitManager).AsSingle().NonLazy();
+            Container.Bind<ResourceService>().FromInstance(_resourceService).AsSingle().NonLazy();
+        }
+    }
+}
